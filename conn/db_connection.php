86cde6636d669313396bb192336bfbdde03dbf88
lyrick <lyrickjonson@gmail.com>
2024-05-06 12:25:31 +0800

    editted db connection

--- conn/db_connection.php
+++ conn/db_connection.php
@@ -5,7 +5,7 @@ DEFINE ('DB_PASSWORD', ''); // change this depending on the db_password
 DEFINE ('DB_HOST', 'localhost');
 
 //DEFINE ('DB_NAME', 'candela_database');
-DEFINE ('DB_NAME', 'if0_36490342_candela_db');
+DEFINE ('DB_NAME', 'candela_db');
 
 $mysqli = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) 
             or die("Unable To Connect: " . mysqli_connect_error());
